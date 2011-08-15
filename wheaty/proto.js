48d8e8cb3dcedbd2c7baa0151e7fc9942f63bb17
zTrix <i@ztrix.me>
2011-08-15 22:31:42 +0800

    proto

--- wheaty/proto.js
+++ wheaty/proto.js
@@ -19,51 +19,57 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 var proto = Object.prototype;
 
 // Implements a forEach much like the one for Array.prototype.forEach, but for
 // any object.
 if (typeof proto.forEach !== 'function') {
-  Object.defineProperty(proto, "forEach", {value: function forEach(callback, thisObject) {
-    var keys = Object.keys(this);
-    var length = keys.length;
-    for (var i = 0; i < length; i++) {
-      var key = keys[i];
-      callback.call(thisObject, this[key], key, this);
-    }
-  }});
+    Object.defineProperty(proto, "forEach", {
+        value: function forEach(callback, thisObject) {
+            var keys = Object.keys(this);
+            var length = keys.length;
+            for (var i = 0; i < length; i++) {
+                var key = keys[i];
+                callback.call(thisObject, this[key], key, this);
+            }
+        }
+    });
 }
 
 // Implements a map much like the one for Array.prototype.map, but for any
 // object. Returns an array, not a generic object.
 if (typeof proto.map !== 'function') {
-  Object.defineProperty(proto, "map", {value: function map(callback, thisObject) {
-    var accum = [];
-    var keys = Object.keys(this);
-    var length = keys.length;
-    for (var i = 0; i < length; i++) {
-      var key = keys[i];
-      accum[i] = callback.call(thisObject, this[key], key, this);
-    }
-    return accum;
-  }});
+    Object.defineProperty(proto, "map", {
+        value: function map(callback, thisObject) {
+            var accum = [];
+            var keys = Object.keys(this);
+            var length = keys.length;
+            for (var i = 0; i < length; i++) {
+                var key = keys[i];
+                accum[i] = callback.call(thisObject, this[key], key, this);
+            }
+            return accum;
+        }
+    });
 }
 
 // Implement extend for easy prototypal inheritance
 if (typeof proto.extend !== 'function') {
-  Object.defineProperty(proto, "extend", {value: function extend(obj) {
-    obj.__proto__ = this;
-    return obj;
-  }});
+    Object.defineProperty(proto, "extend", {
+        value: function extend(obj) {
+            obj.__proto__ = this;
+            return obj;
+        }
+    });
 }
 
 // Implement new for easy self-initializing objects
 if (typeof proto.new !== 'function') {
-  Object.defineProperty(proto, "new", {value: function () {
-    var obj = Object.create(this);
-    if (obj.initialize) obj.initialize.apply(obj, arguments);
-    return obj;
-  }});
+    Object.defineProperty(proto, "new", {
+        value: function () {
+            var obj = Object.create(this);
+            if (obj.initialize) obj.initialize.apply(obj, arguments);
+            return obj;
+        }
+    });
 }
-
